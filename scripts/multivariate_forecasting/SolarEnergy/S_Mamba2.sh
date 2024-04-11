280f9a200055a296c4144519767b3390fc49d5f7
rr <ranr1@uci.edu>
2024-04-10 21:41:25 -0700

    m

---
+++ scripts/multivariate_forecasting/SolarEnergy/S_Mamba2.sh
@@ -0,0 +1,23 @@
+export CUDA_VISIBLE_DEVICES=2
+
+model_name=S_Mamba
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./kaggle/input/solar-al/ \
+  --data_path solar_AL.txt \
+  --model_id solar_96_96 \
+  --model $model_name \
+  --data Solar \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --enc_in 137 \
+  --dec_in 137 \
+  --c_out 137 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --itr 1
+
