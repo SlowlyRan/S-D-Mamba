a904b9632b52de19a15bc5873b25ed1152605cd9
rr <ranr1@uci.edu>
2024-05-08 05:04:52 -0700

    test

--- data_provider/clean.py
+++ data_provider/clean.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-fp = r"C:\Repo\pv_data\Data\clean_data_merged_pwr_nwp_JSGF004.csv"
-df= pd.read_csv(fp)
-df = df[[col for col in df if col != 'power_output'] + ['power_output']]
-df = df.rename(columns={'date_time': "date", "power_output": "OT"})
-df_raw = df
-abnormal = df_raw[df_raw['direction32']==-999].index
-df_raw.date= pd.to_datetime(df_raw.date)
-day = 96
-for i in abnormal:
-    p1 = df_raw.loc[i-day:i+day:2*day]
-    p2 = p1.mean()
-    df_raw.loc[i] = p2
-rp = "C:\Repo\S-D-Mamba\Data/"
-df_raw.to_csv(rp+fp.split("_")[-1],index=False)
+def clean(fp):
+    df= pd.read_csv(fp)
+    df = df[[col for col in df if col != 'power_output'] + ['power_output']]
+    df = df.rename(columns={'date_time': "date", "power_output": "OT"})
+    df_raw = df
+    abnormal = df_raw[df_raw['direction32']==-999].index
+    df_raw.date= pd.to_datetime(df_raw.date)
+    day = 96
+    for i in abnormal:
+        p1 = df_raw.loc[i-day:i+day:2*day]
+        p2 = p1.mean()
+        df_raw.loc[i] = p2
+    rp = "C:\Repo\S-D-Mamba\Data/"
+    df_raw.to_csv(rp+fp.split("_")[-1],index=False)
 
+if __name__ == "__main__":
+    import os
+    rp = r"C:\Repo\pv_data\Data/"
+    ll = os.listdir(rp)
+    for i in ll:
+        print(i)
+        fp = rp+i
+        clean(fp)
